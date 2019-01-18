691e26f1274a9dac656c8890b2779701eb77fb5c
bizzarosanta2 <bizzarosanta2@mac.com>
2019-01-18 11:33:08 -0700

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,6 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+//Jquery test
+$("h1").text("YESSSS!!!! THIS IS THUNDERVISION!")
