ba337a076f3580d859ed7cbd12ebdda6c37160f3
bizzarosanta2 <bizzarosanta2@mac.com>
2019-01-20 10:36:46 -0700

    added dialog module
    and adjusted height

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const dialog = app.dialog;
+// const fs = require('fs'); // Load the File System
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,7 +10,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800, height: 600,
+    width: 700, height: 720,
     webPreferences: { nodeIntegration: true }
   })
 
